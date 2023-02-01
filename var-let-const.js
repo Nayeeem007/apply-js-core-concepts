5f526c627bda21cc5558c366e1b601e979c0fd19
Md. Abu Nayeem <niloy1882@gmail.com>
2023-02-01 20:42:49 +0600

    comment added

--- var-let-const.js
+++ var-let-const.js
@@ -1,9 +1,13 @@
+// you cant redefine let type data twice 
+
 let age = 11;
 // cant redeclare same variable twice in 'let'
 
-let age = 32
+let age = 32;
 age = 12;
 
-const countryName = 'Bangladesh';
+// Cant derive const data type twice, Its a error!
+
+const countryName = 'Bangladesh'; 
 const countryName = 'Indonesia';
 console.log(age, countryName);
